cf902b5b542d775b011e2bf6620f85e1dc6bfcc5
Luis Ballado <luis@madlab.mx>
2023-09-27 14:49:14 -0600

    mod

--- BFS/bfs.cpp
+++ BFS/bfs.cpp
@@ -3,6 +3,7 @@
 #include <vector>
 #include <map>
 #include <unistd.h>
+#include <string>
 
 //Programa BFS con uso directo de la representacion de la matriz
 
@@ -41,70 +42,109 @@ void print_dist(int rows, int cols, std::vector<int>& distance){
   }
 }
 
-void bfs(){
-
-  int inicio_xy = (1,2);
-  int fin_xy = (1,2);
-
-  std::queue<std::pair<int,int>> q;
-  q.push(inicio_xy);
-
+void bfs(std::pair<int,int>& inicio, std::pair<int,int>& fin,int rows,int cols, std::vector<std::vector<char>> grid){
   
+  //indices de la celda en la matriz
+  std::queue<std::pair<int,int>> q;
+  q.push(inicio);
+
+  bool primera = true;
   
   while(!q.empty()){
+
     std::pair<int,int> node = q.front();
+
+    int x = node.first;
+    int y = node.second;
+
+    std::cout << "-NODO-VALUE-" << std::endl;
+    std::cout <<   grid[x][y]   << " " << std::endl;
+    std::cout << "------------" << std::endl;
+    
     q.pop();
 
-    for(int neighbor : neighbors){
-      if (){
-	q.push();
-      }
-
-      if(print){
-	print_dist(rows,cols,distance_bfs);
-      }
+    //obtener posiciones
+    //obtener vecinos a partir de x,y dados
+    std::vector<std::pair<int, int>> neighbors = get_neighbors(x, y, rows, cols);
+    
+    // imprimir los vecinos
+    for (const auto& pair : neighbors) {
+      std::cout << "(" << pair.first << ", " << pair.second << ")";
+      std::cout << " -> " << grid[pair.first][pair.second] << std::endl;
       
+      //si el valor no es una pared # hacer
+      if(grid[pair.first][pair.second] == '#'){
+	
+	//imprimir y salir
+	for(int i=0;i<grid.size();i++){
+	  for(int j = 0; j<grid[i].size();j++){
+	    std::cout << grid[i][j];
+	  }
+	  std::cout<<std::endl;
+	}
+	
+	exit(1);
+	
+      }else{
+	if(grid[pair.first][pair.second] == '0'){
+	  //respecto al pasado sumarle uno
+	  grid[pair.first][pair.second] = int(grid[x][y] + 1);
+	  q.push(pair);
+	}
+      }
+      primera = false;
     }
     
   }
-  
 }
 
-int main(){
 
-  //bool PRINT = false;
+int main(){
   
+  //bool PRINT = false;
+
   int rows;
   int cols;
+  int inicio_x,inicio_y;
+  int fin_x,fin_y;
   
-  //int robot_inicio;
-  //int robot_fin;
-  
+  std::pair<int,int> inicio;
+  std::pair<int,int> fin;
+    
   std::cin >> rows >> cols;
-
-  std::cout << "Filas:    " << rows << std::endl;
-  std::cout << "Columnas: " << cols << std::endl;
 
   //Hacer la matriz del mapa
   std::vector<std::vector<char>> grid(rows, std::vector<char>(cols));
 
+  //construir la matriz
   for(int i=0;i<rows;i++){
     std::string line;
     std::cin >> line;
-
+    
     for(int j=0; j<cols;j++){
       grid[i][j] = line[j];
-      //std::cout << line[j] << std::endl;
-    }  
+    }
+    
   }
   
-  //obtener posiciones
-  std::vector<std::pair<int, int>> neighbors = get_neighbors(1, 2, 10, 10);
+  for(int i=0;i<grid.size();i++){
+    for(int j = 0; j<grid[i].size();j++){
+      std::cout << grid[i][j];
+    }
+    std::cout<<std::endl;
+  }
+  
+  //print_dist(rows,cols);
 
-  // Print the vector of pairs
-  for (const auto& pair : neighbors) {
-    std::cout << "(" << pair.first << ", " << pair.second << ")" << std::endl;
-    std::cout << grid[pair.first][pair.second] << std::endl;
-  }
+  std::cin >> inicio_x >> inicio_y;
+  std::cin >> fin_x >> fin_y;
   
+  inicio = std::make_pair(inicio_x,inicio_y);
+  fin    = std::make_pair(fin_x,fin_y);
+  
+  std::cout << inicio.first << "," << inicio.second << std::endl;
+  std::cout << fin.first << "," << fin.second << std::endl;
+  
+  bfs(inicio,fin,rows,cols,grid);
+
 }
